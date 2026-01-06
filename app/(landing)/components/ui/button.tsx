9a15685332f8462d06d09b78fab8ea74d8871ca2
agusetina-repo <agustina.repo@gmail.com>
2026-01-06 19:12:34 +0700

    develop header and home

---
+++ app/(landing)/components/ui/button.tsx
@@ -0,0 +1,25 @@
+type TButtonProps = {
+    children: React.ReactNode;
+    className?: string;
+    variant?: "primary" | "dark" | "ghost";
+    size?: "small" | "normal" ;
+}& React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button = ({children, className, variant="primary", size ="normal"}: TButtonProps) => {
+    const baseStyles = "inline-flex gap-2 duration-300 justify-center items-center cursor-pointer hover:scale-105";
+    const variantStyles = {
+        primary: "bg-primary text-white hover:bg-primary/85",
+        dark: "bg-dark text-white hover:bg-dark/85",
+        ghost: "bg-transparent hover:bg-gray-100 text-dark"
+    };
+    const sizeStyles = {
+        small: "py-[10px] px-7",
+        normal: "px-9 py-4"
+    };
+
+    return (
+        <button className={`${className} ${baseStyles} ${variantStyles[variant]} ${sizeStyles[size]}`}>{children}</button>
+    )
+}
+
+export default Button;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import HeroSection from "./components/home/hero";
+
+
+
+export default function Home() {
+  return (
+  <main>
+    <HeroSection />
+  </main>
+  );
+}
